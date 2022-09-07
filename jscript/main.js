5bc0184a8bf8ace3c8cf296a35aea71f88f23e6f
Asilbek-17 <asilbekbozorbeyev@gmail.com>
2022-09-07 17:53:25 +0900

    Add all tags

---
+++ jscript/main.js
@@ -0,0 +1,9 @@
+var elThemeButton = document.querySelector(".mode");
+
+elThemeButton.addEventListener("click" , function(){
+    document.body.classList.toggle("theme-dark")
+})
+
+// elThemeButton.addEventListener("click" , function(){
+//     document.body.classList.toggle("theme-dark")
+// })
